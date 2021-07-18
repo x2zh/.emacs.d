91702e425d29032e90eef41e5531ff07ff6a0943
kent <zhangxx_cs@163.com>
2021-07-19 00:19:46 +0800

    feature: init frame size

--- early-init.el
+++ early-init.el
@@ -1,4 +1,8 @@
 ;; -*- lexical-binding: t;-*-
 ;;(add-to-list 'default-frame-alist '(font . "Fira Code-13"))
 
-
+(setq default-frame-alist
+      '((top . 50)
+	(left . 640)
+        (width . 100)
+	(height . 60)))

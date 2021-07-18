779e199ae8b49299d33e95daee7d72d6699de9cb
kent <zhangxx_cs@163.com>
2021-07-19 02:54:18 +0800

    feature(tools): youdao dictionary

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -6,4 +6,12 @@
 	(append '(("\\.http\\'" . restclient-mode))
 		auto-mode-alist)))
 
+
+(use-package youdao-dictionary
+  :bind
+  ("<f6>" . 'youdao-dictionary-search-at-point-posframe))
+
+;;; used by youdao-dictionary
+(use-package posframe)
+
 (provide 'init-tools)
